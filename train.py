e51675a1aaaac27d8c2ece638894098e3eb03805
Kristjan <kristjan.maysoln@gmail.com>
2024-02-19 11:56:08 +0100

    Add validation function

--- train.py
+++ train.py
@@ -1,16 +1,13 @@
 import argparse
-
 import numpy as np
 from dataset_loader import DatasetFolder
 from torch.utils.data import DataLoader
-from torchvision import transforms
 from network import UNet
 import torch
 from tqdm import tqdm
 import os
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
-from torchmetrics.classification import BinaryJaccardIndex
 import datetime
 import time
 import matplotlib.pyplot as plt
@@ -29,10 +26,10 @@ def calculate_iou(predictions, masks):
 
 
 # Base training function
-def train(trainloader, model, device, optimizer):
+def train(trainloader, model, device, optimizer, loss_function):
 
     model.train()
-    avg_loss = 0
+    avg_loss = []
 
     for images, masks, img_paths, mask_paths in tqdm(trainloader):
 
@@ -43,19 +40,49 @@ def train(trainloader, model, device, optimizer):
         # out = softmax(predictions)
         # out = sigmoid(predictions)
         
-        loss = l_bce(predictions, masks) # Calculate the error of the current batch
-        avg_loss += loss.cpu().detach().numpy()
+        loss = loss_function(predictions, masks) # Calculate the error of the current batch
+        avg_loss.append(loss.cpu().detach().numpy())
 
         loss.backward() # Calculate gradients with backpropagation
         optimizer.step() # optimize weights for the next batch
 
-    avg_loss = avg_loss/len(train_data)
+    avg_loss = np.mean(avg_loss)
     return avg_loss
 
-# Inference (validation/testing) function
-# TODO: Add option to store the resulting images (and scores/loss?)
 
+# Base validation/testing function
+def test(testloader, model, device, loss_function):
+    
+    model.eval()
+    iou = []
+    avg_val_loss = []
 
+    with torch.no_grad():
+        # for images, masks, img_paths, mask_paths in tqdm(testloader):
+        for images, masks, img_paths, mask_paths in tqdm(testloader):
+            images, masks = images.to(device), masks.to(device)
+
+            predictions = model(images)
+            out = torch.sigmoid(predictions)
+            
+            # Calculate IoU for training data
+            predicted_masks_bin = torch.sigmoid(out) > 0.5
+            # predicted_masks_bin = Sigmoid(out) > 0.5
+            iou.append(calculate_iou(predicted_masks_bin, masks).cpu().numpy())
+
+            avg_val_loss.append(loss_function(predictions, masks).cpu().detach().numpy())
+
+            # plt.imshow(masks[0].cpu())
+            # plt.show()
+            # plt.imshow(predicted_masks_bin[0].cpu())
+            # plt.show()
+
+    iou = np.mean(iou)  # Calculate the total accuracy of the model
+    avg_val_loss = np.mean(avg_val_loss)
+
+    return iou, avg_val_loss
+
+# TODO: Add inference which stores the resulting masks (and scores/loss?)
 
 # MAIN
     
@@ -67,15 +94,21 @@ if __name__ == "__main__":
     # options.add_argument('--train', action='store_true', help='Train the model.')
     # options.add_argument('--test', action='store_true', help='Test the model.')
     
-    options.add_argument('--traincsv', default='dataset/train.csv', help='directory of the train CSV')
-    options.add_argument('--testcsv', default='dataset/test.csv', help='directory of the test CSV')
+    # options.add_argument('--traincsv', default='dataset/train.csv', help='directory of the train CSV')
+    # options.add_argument('--testcsv', default='dataset/test.csv', help='directory of the test CSV')
+    # options.add_argument('--valcsv', default='dataset/val.csv', help='directory of the validation CSV')
+    # TODO: DELETE AFTER TESTING
+    options.add_argument('--traincsv', default='dataset/val.csv', help='directory of the train CSV')
+    options.add_argument('--testcsv', default='dataset/val.csv', help='directory of the test CSV')
     options.add_argument('--valcsv', default='dataset/val.csv', help='directory of the validation CSV')
     
     options.add_argument('--batchsize', type=int, default=1, help='batch size')
     # options.add_argument('--imagesize', type=int, default=(512,384), help='size of the image (height, width)')
     # options.add_argument('--imagesize', type=int, default=(622, 1104), help='size of the image (height, width)')
-    options.add_argument('--imagesize', type=int, default=(624, 1104), help='size of the image (height, width)') # Needs to be divisible by 16
-    options.add_argument('--epochs', type=int, default=150, help='number of training epochs')
+    # options.add_argument('--imagesize', type=int, default=(624, 1104), help='size of the image (height, width)') # Needs to be divisible by 16
+    # TODO: DELETE AFTER TESTING
+    options.add_argument('--imagesize', type=int, default=(256, 256), help='size of the image (height, width)') # Needs to be divisible by 16
+    options.add_argument('--epochs', type=int, default=2, help='number of training epochs')
     
     opt = options.parse_args()
 
@@ -83,6 +116,7 @@ if __name__ == "__main__":
     PRE__STD = [0.5, 0.5, 0.5]
     
     # Define transforms for dataset augmentation
+    # TODO: Add transformations
     image_and_mask_transform_train=A.Compose([A.Resize(opt.imagesize[0], opt.imagesize[1]),
                                               A.HorizontalFlip(p=0.5),
                                               A.VerticalFlip(p=0.5),
@@ -110,6 +144,7 @@ if __name__ == "__main__":
     valloader = DataLoader(val_data, opt.batchsize, shuffle=False)
     testloader = DataLoader(test_data, opt.batchsize, shuffle=False)
 
+    print('--------- Stats --------')
     print(f"Train dataset stats: number of images: {len(train_data)}")
     print(f"Validation dataset stats: number of images: {len(val_data)}")
     print(f"Test dataset stats: number of images: {len(test_data)}")
@@ -125,17 +160,16 @@ if __name__ == "__main__":
     # Initialize the loss function and iou
     # l_ce = torch.nn.CrossEntropyLoss()
     l_bce = torch.nn.BCEWithLogitsLoss()
-    jaccard = BinaryJaccardIndex(threshold=0.5).to(device)
+    # jaccard = BinaryJaccardIndex(threshold=0.5).to(device)
 
     # Init softmax layer
     # softmax = torch.nn.Softmax(dim=1)
-    sigmoid = torch.nn.Sigmoid()
+    # sigmoid = torch.nn.Sigmoid()
 
     # Conversion from BGR to single channel grayscale images - for masks
-    to_gray = torch.nn.Conv2d(in_channels=3, out_channels=1, kernel_size=1)
-    gray_kernel = torch.FloatTensor([[[[0.114]], [[0.587]], [[0.299]]]])
-    to_gray.weight = torch.nn.Parameter(gray_kernel, requires_grad=False)
-
+    # to_gray = torch.nn.Conv2d(in_channels=3, out_channels=1, kernel_size=1)
+    # gray_kernel = torch.FloatTensor([[[[0.114]], [[0.587]], [[0.299]]]])
+    # to_gray.weight = torch.nn.Parameter(gray_kernel, requires_grad=False)
 
     # Initialize the optimizer
     optimizer = torch.optim.Adam(model.parameters(), lr=0.0001, betas=(0.9, 0.999))
@@ -144,80 +178,62 @@ if __name__ == "__main__":
     val_loss_over_time = []
     val_iou_over_time = []
 
-    best_iou=0
+    best_iou = 0
 
     start_time = time.time()
 
-    break_flag = False
+    print('------- Training -------')
 
     for epoch in range(opt.epochs):
-        print("Training epoch: {}/{}".format(epoch+1, opt.epochs))
-
 
         try:
             # Train
-            avg_loss = train(trainloader, model, device, optimizer)
-            print("Epoch {}: average  train loss: {:.7f}".format(epoch+1, avg_loss))
+            avg_loss = train(trainloader, model, device, optimizer, l_bce)
             train_loss_over_time.append(avg_loss)
 
             # Validation
-            model.eval()
-            iou = []
-            avg_val_loss = []
-
-            with torch.no_grad():
-                # for images, masks, img_paths, mask_paths in tqdm(testloader):
-                for images, masks, img_paths, mask_paths in testloader:
-                    images, masks = images.to(device), masks.to(device)
-
-                    predictions = model(images)
-                    out = sigmoid(predictions)
-                    
-                    # Calculate IoU for training data
-                    # iou.append(jaccard(preds=out, target=masks).cpu().numpy())
-                    predicted_masks_bin = torch.sigmoid(out) > 0.5
-                    iou.append(calculate_iou(predicted_masks_bin, masks).cpu().numpy())
-
-                    avg_val_loss.append(l_bce(predictions, masks).cpu().detach().numpy())
-
-                    # plt.imshow(masks[0].cpu())
-                    # plt.show()
-                    # plt.imshow(predicted_masks_bin[0].cpu())
-                    # plt.show()
-                    # raise KeyboardInterrupt
-
-            # iou = iou/len(test_data)  # Calculate the total accuracy of the model
-            iou = np.mean(iou)  # Calculate the total accuracy of the model
-            # avg_val_loss = avg_val_loss/len(test_data)
-            avg_val_loss = np.mean(avg_val_loss)
+            # TODO: Add better performance measures
+            iou, avg_val_loss = test(testloader, model, device, l_bce)
             val_iou_over_time.append(iou)
             val_loss_over_time.append(avg_val_loss)
 
-            # save network weights when the accuracy is greater than the best_acc
-            if iou>best_iou:
+            # Save network weights if better than previous best
+            if iou > best_iou:
                 torch.save({'epoch': epoch, 'state_dict': model.state_dict()}, './output/CNN_weights.pth')
                 best_iou = iou
-                print(f'New best')
+                print(f'New best, saving weights')
 
-            print("Average IOU: {:.7f}     Best IOU: {:.7f}, val loss: {:.7f}".format(iou, best_iou, avg_val_loss))
+            line = f'Epoch {epoch+1}/{opt.epochs}: Train loss: {avg_loss:.7f}, val. loss: {avg_val_loss:.7f}, val. IOU: {iou:.7f}, best val. IOU: {best_iou:.7f}'
+            print(line)
 
             with open(logfile, 'a') as file:
                 # Convert data to a string and write it to the file
                 row = f'Epoch {epoch}: Train loss: {avg_loss}, val loss: {avg_val_loss}, val iou: {iou}' '\n'
                 file.write(row)
-
-            if break_flag:
-                break
             
-        except KeyboardInterrupt:
-            break_flag = True
-            print("Keyboard interrupt: Breaking after next validation")
+            # TODO: Implement early stop
+            # TODO: Implement scheduler
 
-    print('\n----------------------------------------------------------------')
-    # Print total training time
+        except KeyboardInterrupt:
+            with open(logfile, 'a') as file:
+                # Convert data to a string and write it to the file
+                row = f'Stopping due to keyboard interrupt' '\n'
+                print("Stopping due to keyboard interrupt")
+                file.write(row)
+            break
+
+    
+    print('----- End training ------')
+    # TODO: Add logging function
+    
+    # Print info
     end_time = time.time()
     elapsed_time = datetime.timedelta(seconds=(end_time - start_time))
-    print(f"Total training time: {elapsed_time}")
+    with open(logfile, 'a') as file:
+        # Convert data to a string and write it to the file
+        row = f'Total training time: {elapsed_time}' '\n'
+        print(row)
+        file.write(row)
 
     # Plot loss over time
     plt.figure(figsize=(15, 10))
